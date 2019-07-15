6631c3391132c00dd4a29101cbb2c3bee6a33176
catharina kaltenbach <catharina904@outlook.com>
2019-07-15 20:50:37 +0200

    scaffold crud articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_132231) do
+ActiveRecord::Schema.define(version: 2019_07_15_165207) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
